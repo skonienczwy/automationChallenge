e4a40a7bd899f16aa69c2ffcaa10178e02f89d91
skonienczwy <skonienczwy@hotmail.com>
2021-08-01 23:10:26 +0200

    Add files via upload

---
+++ test.tf
@@ -0,0 +1,76 @@
+# Configure the provider, region and your access/secret keys
+provider "aws" {
+  region     = var.region
+  access_key = var.accessKey
+  secret_key = var.secretKey
+}
+
+resource "aws_instance" "cgi-server" {
+    ami = var.ami
+    instance_type = var.instanceType
+    key_name      = "main-key"
+    security_groups = [aws_security_group.web.name]
+    user_data = <<-EOF
+                #!bin/bash
+                sudo apt update -y
+                sudo apt install docker.io -y
+                sudo apt install docker-compose -y
+                sudo systemctl start docker.io
+                sudo systemctl start docker-compose
+                sudo git clone https://github.com/skonienczwy/automationChallenge.git
+                sudo docker run --name docker-nginx -p 80:80 -d -v /automationChallenge:/usr/share/nginx/html nginx
+                EOF
+    
+
+    tags = {
+      "Name" = "DockerInstance"
+    }
+
+}
+
+resource "aws_security_group" "web" {
+    name = "web-security-group"
+    description = "Allow access to the Web server"
+
+    
+
+    ingress {      
+      description = "Allow SSH"
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    } 
+
+    ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+
+
+
+}
+#output "instance_public_dns" {
+ #   value = "aws_instance.web.public_dns"
+  #  }
---
+++ variable.tf
@@ -0,0 +1,32 @@
+variable  "accessKey"{
+    type = string
+    default = "AKIAWGA5VBH4NAUNWI5W"
+}
+
+variable  "secretKey"{
+    type = string
+    default = "830uF16/yDDuamzjsmr3wyWePy0uY+HtVgC7w7Bl"
+}
+
+variable  "region"{
+    type = string
+    default = "eu-central-1"
+}
+
+variable  "ami"{
+    type = string
+    default = "ami-05f7491af5eef733a"
+}
+
+variable  "instanceType"{
+    type = string
+    default = "t2.micro"
+    
+}
+
+
+variable "publicKey"{
+    type = string
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCExYD43mXU0xio5F1pwnBl+8jt8x3APpp/5LTufsdDp8eptBS4Wb1SSswHopClBOs8/dxu6k4Z92kiYfnusxvIJ48vb6zHN52j2BOZBQkgos8G6jqQfcwn62TklBatwsy0wL7acPETX0wBtRmxQ8F7JMQbrqLjSSv2guENChQ+73uDRvI5Ul+VA2plNFIoKUVk+EvSTjjgUXb05Ah65lmf4cGqcKaS3HbUJ8sNhzUhPE6G2FAK//5n06oYe5N2inZbDzEBkM7XzbdGZuJ8kSv1R34yxBGLW+qAf8epq8ztF0wrb0ZV6dGYQDEom9/dgqfVq5ydyGzsNF9YTgMM1GNn imported-openssh-key"
+                
+}
